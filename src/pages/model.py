058bb15ce8d077e8530327f8651f36eb1e081b88
Matthew Cronin <matthew.j.cronin@gmail.com>
2021-08-21 18:18:21 -0500

    Take country, region, sub_region as *args

--- src/pages/model.py
+++ src/pages/model.py
@@ -4,6 +4,6 @@ import streamlit as st
 from src.model import covid_bayes
 
 
-def write(df: pd.DataFrame) -> None:
+def write(df: pd.DataFrame, country: str, state: str, sub_region: str) -> None:
     st.title('COVID-19 infeciton likelihood estimation')
     return None
