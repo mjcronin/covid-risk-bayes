44c9c2ad9284b56849ab03fd7f53e1702a81c99d
Matthew Cronin <matthew.j.cronin@gmail.com>
2021-08-21 22:57:24 -0500

    Download vaccine data from CCI

--- src/data/data.py
+++ src/data/data.py
@@ -1,12 +1,12 @@
 import os
 import datetime as dt
-from typing import List, Tuple, Optional
+from typing import List, Tuple, Optional, Dict
 
 import pandas as pd
 import numpy as np
 
 
-def load(today: dt.date, data_dir: str='./data/') -> pd.DataFrame:
+def load_cases(today: dt.date, data_dir: str='./data/') -> pd.DataFrame:
     """
     Load last 15 days of JHU COVID data.
 
@@ -23,12 +23,30 @@ def load(today: dt.date, data_dir: str='./data/') -> pd.DataFrame:
     downloaded = [f[:-4] for f in raw_files if f[-4:]=='.csv']
     to_download = [f for f in last_15 if f not in downloaded]
 
-    download_and_save(to_download, data_dir=data_dir)
+    # Download data from remote repositories
+    download_and_save_JHU(to_download, data_dir=data_dir)
+    download_and_save_CCI(data_dir)
+
     df = load_and_concat(last_15, data_dir=data_dir, today=today)
 
     return df
 
-def download_and_save(to_download: List[str], data_dir: str='./data/') -> None:
+
+def load_vaccinations(
+    today: dt.date, data_dir: str='./data/'
+) -> Dict[str, pd.DataFrame]:
+    """
+    Load last 15 days of CCI COVID vaccinationdata.
+
+    Args:
+        today (dt.date)
+    Returns:
+        df (Dict[str, pd.DataFrame]): DataFrame containing the last 15 days of CCI
+            COVID vaccination data for 'US' and 'Global'
+    """
+
+
+def download_and_save_JHU(to_download: List[str], data_dir: str='./data/') -> None:
     """Download last 15* daily CSVs from JHU COVID tracker if not stored locally
     
     \* 15 days are pulled to allow a diff() operation to calculate new cases over the 
@@ -51,6 +69,22 @@ def download_and_save(to_download: List[str], data_dir: str='./data/') -> None:
 
     return None
 
+
+def download_and_save_CCI(data_dir: str='./data/raw/') -> None:
+    """Download the latest US and Global vaccination data from the CCI repo and save"""
+    data_dir += 'raw/vaccinations/'
+    url_us = (
+        'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables'
+        '/vaccine_data/us_data/hourly/vaccine_people_vaccinated_US.csv'
+    )
+    url_global = (
+        'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables'
+        '/vaccine_data/global_data/time_series_covid19_vaccine_global.csv'
+    )
+    pd.read_csv(url_us).to_csv(data_dir+'vaccinations_us.csv')
+    pd.read_csv(url_us).to_csv(data_dir+'vaccinations_global.csv')
+
+    return None
 
 def load_and_concat(
     last_15: List[str], data_dir: str='./data/', today: dt.date=None
@@ -152,6 +186,5 @@ def subset_data(
         subset = subset.iloc[1:].reset_index(drop=True)
     except:
         subset = None
-        # st.write("## Data not reported at this granularity in the last 14 days.")
 
     return subset
