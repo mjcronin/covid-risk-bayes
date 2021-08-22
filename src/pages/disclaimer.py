d0ae49c91a7c37109af36997bac4c0bad6e479e1
Matthew Cronin <matthew.j.cronin@gmail.com>
2021-08-22 17:24:11 -0500

    Select relevant subset of README

--- src/pages/disclaimer.py
+++ src/pages/disclaimer.py
@@ -1,2 +1,13 @@
+import streamlit as st
+
 def write():
-    return
+    with open('README.md', 'r') as f:
+        body = f.readlines()
+    
+    disclaimer = [body[1]]
+    for line in body[2:3]:
+        disclaimer.append(line)
+    disclaimer = ''.join(disclaimer)
+    st.write(disclaimer)
+
+    return None
