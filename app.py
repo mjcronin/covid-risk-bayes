dbf8a9dae667f2112945fd7aca3070d61859799b
Matthew Cronin <matthew.j.cronin@gmail.com>
2021-08-21 23:54:08 -0500

    Break sidebar out inot own function

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 
 import os
 import datetime as dt
-from typing import List
+from typing import List, Tuple
 
 import streamlit as st
 import yaml
 import pandas as pd
 
-from src.pages import home, model, disclaimer
+from src.pages import home, model, disclaimer, about
 from src.data import data
 
 
@@ -17,15 +17,60 @@ with open('./config.yaml', 'r') as f:
 _PAGES = {
     'Home': home,
     'Model': model,
-    'About': None,
+    'About': about,
     'Disclaimer': disclaimer
 }
 
+
 def main():
     st.write('Proof of concept - see Disclaimer page')
-    with st.spinner('Loading data...'):
-        df, countries = load_data(dt.date.today(), _CONFIG['data_dir'])
 
+    with st.spinner(text='Loading data...'):
+        df, vacc_data, countries = load_data(dt.date.today(), _CONFIG['data_dir'])
+    # Write sidebar and return user inputs
+    page, country, state, sub_region = write_sidebar(df, countries)
+    # Write main page content
+    args = {
+        'Home': [],
+        'Model': [df, vacc_data, country, state, sub_region],
+        'About': [],
+        'Disclaimer': []
+    }
+    _PAGES[page].write(*args[page])
+
+    return None
+
+
+@st.cache
+def load_data(today: dt.date, data_dir: str='./data/') -> pd.DataFrame:
+    """Load latest COVID data from JHU Github repo
+
+    Args:
+        today (dt.date): Todays's date
+    Returns:
+        df (pd.DataFrame): Last 14 days of daily covid incidence per 100k population by
+            geography.
+    """
+    df = data.load_cases(today, data_dir)
+    countries = data.get_regions(df)
+
+    vacc_data = data.load_vaccinations(data_dir=data_dir)
+    
+    return df, vacc_data, countries
+
+
+def write_sidebar(df: pd.DataFrame, countries: List[str]) -> Tuple[str]:
+    """Populate app sidebar and return user inputs
+    
+    Args:
+        df (pd.DataFrame): Last 14 days' JHU COVID data
+        Countries (List[str]): List of unique countries in df.Country_Region
+    Returns:
+        page (str): Selected page
+        country (str): Selected country
+        region (str): Selected state/region
+        sub_region (str): Selecetd county/sub-region
+    """
     sidebar_navigation = st.sidebar.container()
     sidebar_settings = st.sidebar.container()
     sidebar_info = st.sidebar.container()
@@ -70,30 +115,7 @@ def main():
             """App developed by Matthew Cronin.\n\nQuestions/comments? Please email matthew.j.cronin@gmail.com
             """)
 
-    args = {
-        'Home': [],
-        'Model': [df, country, state, sub_region],
-        'About': [],
-        'Disclaimer': []
-    }
-    _PAGES[page].write(*args[page])
-
-    return None
-
-@st.cache
-def load_data(today: dt.date, data_dir: str='./data/') -> pd.DataFrame:
-    """Load latest COVID data from JHU Github repo
-
-    Args:
-        today (dt.date): Todays's date
-    Returns:
-        df (pd.DataFrame): Last 14 days of daily covid incidence per 100k population by
-            geography.
-    """
-    df = data.load_cases(today, data_dir)
-    countries = data.get_regions(df)
-    
-    return df, countries
+    return (page, country, state, sub_region)
 
 
 def get_regions(df: pd.DataFrame, country: str):
